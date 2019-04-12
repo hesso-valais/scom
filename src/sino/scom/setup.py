ed3ee788ce2e75765be4135b8642c95efec9d125
thomas.sterren <thomas.sterren@hevs.ch>
2019-04-12 08:40:25 +0200

    Using cythonize()

--- src/sino/scom/setup.py
+++ src/sino/scom/setup.py
@@ -4,14 +4,17 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+from Cython.Build import cythonize
 
 #
 # Call in shell: python setup.py build_ext --inplace
 #
 setup(
     name="scomlib",
-    ext_modules=[Extension("sino.scom.baseframe", ["sino/scom/baseframe.pyx", "sino/scom/scomlib/scom_data_link.c"]),
-                 Extension("sino.scom.property", ["sino/scom/property.pyx", "sino/scom/scomlib/scom_property.c"])],
+    ext_modules=cythonize(
+                    [Extension("sino.scom.baseframe", ["sino/scom/baseframe.pyx", "sino/scom/scomlib/scom_data_link.c"]),
+                     Extension("sino.scom.property", ["sino/scom/property.pyx", "sino/scom/scomlib/scom_property.c"])]
+                        ),
     include_dirs=['sino/scom', ],
     cmdclass={'build_ext': build_ext}
 )
