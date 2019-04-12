0d21abdd267361f70ffc677181a766e54975f6eb
thomas.sterren <thomas.sterren@hevs.ch>
2019-04-12 08:35:20 +0200

    Added files to clean before build

--- scripts/build-scomlib.sh
+++ scripts/build-scomlib.sh
@@ -5,4 +5,8 @@ WORK_DIR=${0%/*}
 # Start build in the 'src' folder
 cd ${WORK_DIR}/../src
 
+rm -f sino/scom/*.cpp
+rm -f sino/scom/*.pyd
+rm -f sino/scom/*.so
+
 pipenv run python ./sino/scom/setup.py build_ext --inplace
