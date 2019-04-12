e2d69de75a297e9d8a152a923ef0d3ca33305ea2
Thomas Sterren <thomas.sterren@gmail.com>
2019-04-12 11:14:11 +0200

    Update script file

--- scripts/build-scomlib.sh
+++ scripts/build-scomlib.sh
@@ -2,9 +2,14 @@
 
 WORK_DIR=${0%/*}
 
+# Be sure virtual environment is set up
+cd ${WORK_DIR}/..
+#pipenv install
+
 # Start build in the 'src' folder
 cd ${WORK_DIR}/../src
 
+# Remove generated files to force a complete build
 rm -f sino/scom/*.cpp
 rm -f sino/scom/*.pyd
 rm -f sino/scom/*.so
