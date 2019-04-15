110a3a60e30a7371a428db4dc10ac06375ecf153
Thomas Sterren <thomas.sterren@gmail.com>
2019-04-15 12:30:15 +0200

    Updated build script to reflect changes with package path in setup script

--- scripts/build-scomlib.sh
+++ scripts/build-scomlib.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
-WORK_DIR=${0%/*}
+SCRIPT_DIR=${0%/*}
+WORK_DIR=${SCRIPT_DIR}/../src/sino/scom
 
 # Be sure virtual environment is set up
-cd ${WORK_DIR}/..
+cd ${SCRIPT_DIR}/..
 #pipenv install
 
 # Start build in the 'src' folder
-cd ${WORK_DIR}/../src
+cd ${SCRIPT_DIR}/..
 
 # Remove generated files to force a complete build
-rm -f sino/scom/*.cpp
-rm -f sino/scom/*.pyd
-rm -f sino/scom/*.so
+rm -f src/sino/scom/*.cpp
+rm -f src/sino/scom/*.pyd
+rm -f src/sino/scom/*.so
 
-pipenv run python ./sino/scom/setup.py build_ext --inplace
+# Build the extension
+pipenv run python ./src/sino/scom/setup.py build_ext --inplace
+
+# Move it to right location 'eq.: sino/scom'
+mv -f baseframe.*so ${WORK_DIR}/
+mv -f property.*so ${WORK_DIR}/
